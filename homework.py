0d12bc0dc6d0c01c0de07f37359550b50c3cc206
ln9var <97240127+ln9var@users.noreply.github.com>
2022-04-05 14:19:55 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -126,7 +126,7 @@ def main() -> None:
 if __name__ == '__main__':
     
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    homework.run(host='0.0.0.0', port=port)
 
     # Создание и настройка логгера
     logger = logging.getLogger(__name__)
