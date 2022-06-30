4dedeae999730899a989904ce5b0f1828c91cee7
ln9var <97240127+ln9var@users.noreply.github.com>
2022-06-30 21:24:25 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -136,5 +136,9 @@ if __name__ == '__main__':
         '%(asctime)s - %(levelname)s - %(message)s'
     )
     handler.setFormatter(formatter)
+    
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
     main()
